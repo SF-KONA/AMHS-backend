926978a8284434b0f91dbd9d71e1ff3c99bd813c
kimyoungbok1109-blip <kimyoungbok1109@gmail.com>
2026-04-16 12:24:07 +0900

    feat: login API 구현 (#12)

---
+++ src/test/java/com/smartfactory/predictive/backend/EncodeTest.java
@@ -0,0 +1,11 @@
+package com.smartfactory.predictive.backend;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class EncodeTest {
+
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("1234"));
+    }
+}
